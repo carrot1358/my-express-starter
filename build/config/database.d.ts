e3a17a14d69d7fa93b7c26a9a9d47ce6580f2d9f
nattapad sa <carrot1358@gmail.com>
2025-08-22 15:54:37 +0700

    Remove build artifacts and update .gitignore to exclude the build directory

--- build/config/database.d.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-declare global {
-    var __db__: PrismaClient;
-}
-declare let prisma: PrismaClient;
-export { prisma };
-//# sourceMappingURL=database.d.ts.map
--- build/src/utils/response.d.ts
+++
@@ -1,9 +0,0 @@
-import { ApiResponse } from '../interfaces/common.interface';
-export declare class ResponseUtils {
-    static success<T>(data: T, message?: string): ApiResponse<T>;
-    static error(error: string, message?: string): ApiResponse;
-    static created<T>(data: T, message?: string): ApiResponse<T>;
-    static updated<T>(data: T, message?: string): ApiResponse<T>;
-    static deleted(message?: string): ApiResponse;
-}
-//# sourceMappingURL=response.d.ts.map
